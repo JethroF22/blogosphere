4f18d9ceec6e5066b602949530f0ce993a719fc6
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-06 21:37:46 +0200

    Add `onChange` handlers to all input fields; add `submit` button

--- src/components/RegistrationPage.jsx
+++ src/components/RegistrationPage.jsx
@@ -8,23 +8,72 @@ class RegistrationPage extends Component {
     email: ""
   };
 
+  onEmailChange = e => {
+    const email = e.target.value;
+    this.setState(() => ({
+      email
+    }));
+  };
+
+  onUsernameChange = e => {
+    const username = e.target.value;
+    this.setState(() => ({
+      username
+    }));
+  };
+
+  onPasswordChange = e => {
+    const password = e.target.value;
+    this.setState(() => ({
+      password
+    }));
+  };
+
+  onConfirmPasswordChange = e => {
+    const confirmPassword = e.target.value;
+    this.setState(() => ({
+      confirmPassword
+    }));
+  };
+
   render() {
     return (
       <div>
         <h1>Create An Account</h1>
         <form>
           <label htmlFor="username">Username: </label>
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.onUsernameChange}
+          />
           <br />
           <label htmlFor="email">Email: </label>
-          <input type="text" name="email" />
+          <input
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={this.onEmailChange}
+          />
           <br />
           <label htmlFor="password">Password: </label>
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.onPasswordChange}
+          />
           <br />
           <label htmlFor="confirmPassword">Confirm Password: </label>
-          <input type="password" name="confirmPassword" />
+          <input
+            type="password"
+            name="confirmPassword"
+            value={this.state.confirmPassword}
+            onChange={this.onConfirmPasswordChange}
+          />
           <br />
+          <button type="submit">Submit</button>
         </form>
       </div>
     );
