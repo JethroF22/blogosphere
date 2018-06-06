d9526ec9fb830305c96633e9b79e0152e307cf22
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-06 21:25:33 +0200

    Create `RegistrationPage` component

---
+++ src/components/RegistrationPage.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class RegistrationPage extends Component {
+  state = {
+    password: "",
+    confirmPassword: "",
+    username: "",
+    email: ""
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Create An Account</h1>
+        <form>
+          <label htmlFor="username">Username: </label>
+          <input type="text" name="username" />
+          <br />
+          <label htmlFor="email">Email: </label>
+          <input type="text" name="email" />
+          <br />
+          <label htmlFor="password">Email: </label>
+          <input type="password" name="password" />
+          <br />
+          <label htmlFor="confirmPassword">Email: </label>
+          <input type="password" name="confirmPassword" />
+          <br />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegistrationPage;
