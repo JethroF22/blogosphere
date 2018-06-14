7889f1ffa751c868c7b1711a3ef3b593eb197073
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-14 13:12:00 +0200

    Connect to redux store

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,16 @@
 import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
+import configureStore from "./store/configureStore";
 import AppRouter from "./routes/AppRouter";
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const store = configureStore();
+const root = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(root, document.getElementById("app"));
