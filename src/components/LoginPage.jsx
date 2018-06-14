22e39b7ab587bc605042c0e57d93f084167daaff
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-14 19:31:58 +0200

    Add `onChange` handlers to `email` and `password` fields

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -8,16 +8,40 @@ class LoginPage extends Component {
     email: ""
   };
 
+  onEmailChange = e => {
+    const email = e.target.value;
+    this.setState(() => ({
+      email
+    }));
+  };
+
+  onPasswordChange = e => {
+    const password = e.target.value;
+    this.setState(() => ({
+      password
+    }));
+  };
+
   render() {
     return (
       <div>
         <h1>Create An Account</h1>
         <form>
           <label htmlFor="email">Email: </label>
-          <input type="text" name="email" value={this.state.email} />
+          <input
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={this.onEmailChange}
+          />
           <br />
           <label htmlFor="password">Password: </label>
-          <input type="password" name="password" value={this.state.password} />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.onPasswordChange}
+          />
           <br />
           <button type="submit">Submit</button>
         </form>
