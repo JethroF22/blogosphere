6e26ce4a704ea9796e35d4ebe4ffb14f29374677
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-14 02:41:22 +0200

    Configure server for testing

--- server/server.js
+++ server/server.js
@@ -1,4 +1,11 @@
-require("dotenv").config();
+const env = process.env.NODE_ENV || "development";
+
+if (env === "development") {
+  require("dotenv").config();
+} else if (env === "test") {
+  require("dotenv").config({ path: "./.env.test" });
+}
+
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -12,6 +19,8 @@ app.use(bodyParser.json());
 app.use(express.static(publicPath));
 app.use("/auth", auth);
 
-app.listen(3000, () => {
-  console.log("Server is live on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is live on port ${process.env.PORT}`);
 });
+
+module.exports = app;
