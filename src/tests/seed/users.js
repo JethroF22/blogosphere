04fb239917af84fd61bed8d5a56bc30a398b6e80
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-15 06:02:39 +0200

    Create seed user data for tests

---
+++ src/tests/seed/users.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    email: "userone@gmail.com",
+    username: "User One",
+    password: "userone1",
+    confirmPassword: "userone1"
+  },
+  {
+    email: "invalidemail",
+    username: "User Two",
+    password: "usertwo2"
+  }
+];
