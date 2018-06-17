1e5cfc1e70a466b1ca690c3dc03be615b210f187
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-17 13:34:41 +0200

    Create POST /blog/create

---
+++ server/routes/blog.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const _ = require("lodash");
+
+const { BlogPost } = require("../models/blogPost");
+const { authenticate } = require("../middleware/authenticate");
+
+const router = express.Router();
+
+router.post("/create", authenticate, (req, res) => {
+  const body = req.body;
+  const post = new BlogPost({
+    ...body,
+    createdAt: new Date(),
+    author: {
+      _id: req.user._id,
+      name: req.user.username
+    }
+  });
+
+  post
+    .save()
+    .then(doc => {
+      res.send(doc);
+    })
+    .catch(() => {
+      res.status(400).send();
+    });
+});
