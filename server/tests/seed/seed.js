a2b93165c5df7083512fcab12235f210af57542e
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-18 22:42:09 +0200

    Create `populateBlogPosts`` function

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,7 +1,9 @@
 const { ObjectID } = require("mongodb");
 const jwt = require("jsonwebtoken");
+const slugify = require("slugify");
 
 const User = require("../../models/user");
+const BlogPost = require("../../models/blogPost");
 
 const userOneID = new ObjectID();
 const userTwoID = new ObjectID();
@@ -35,22 +37,28 @@ const blogPosts = [
   {
     title: "Test 1",
     body: "This is a test",
-    createdAt: 298342938
+    createdAt: 298342938,
+    slug: slugify("Test 1")
   },
   {
     title: "Test 2",
     body: "This is also test",
-    createdAt: 298342938
-  },
-  {
-    title: "Invalid Post",
-    createdAt: null,
-    body: 12932
+    createdAt: 298342938,
+    slug: slugify("Test 2")
   }
 ];
+
+const populateBlogPosts = done => {
+  BlogPost.remove({})
+    .then(() => {
+      return BlogPost.insertMany(blogPosts);
+    })
+    .then(() => done());
+};
 
 module.exports = {
   users,
   populateUsers,
-  blogPosts
+  blogPosts,
+  populateBlogPosts
 };
