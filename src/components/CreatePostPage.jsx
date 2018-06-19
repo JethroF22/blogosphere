e458b298d003b25d35b255e76cb9addaa529c6d4
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-19 19:36:38 +0200

    Change action status to `In progress` from `onSubmit` method

--- src/components/CreatePostPage.jsx
+++ src/components/CreatePostPage.jsx
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Textarea from "react-textarea-autosize";
 import _ from "lodash";
+import slugify from "slugify";
 
 import { createArticle } from "../actions/blog";
+import setActionStatus from "../actions/status";
 
 class CreateBlogPostPage extends Component {
   state = {
@@ -40,18 +42,10 @@ class CreateBlogPostPage extends Component {
     if (!(this.state.body === "" && this.state.title === "")) {
       const blogPost = _.pick(this.state, ["title", "body", "coverPhotoURL"]);
       const token = localStorage.getItem("token");
-
-      console.log("post:", blogPost);
-      console.log("token:", token);
-
-      this.props
-        .createArticle(blogPost, token)
-        .then(() => {
-          console.log("Article created");
-        })
-        .catch(() => {
-          console.log("DB error");
-        });
+      this.props.setActionInProgress();
+      this.props.createArticle(blogPost, token).then(() => {
+        console.log(this.props.actionStatus);
+      });
     } else {
       const errors = {};
       ["title", "body"].forEach(field => {
@@ -104,11 +98,16 @@ class CreateBlogPostPage extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  actionStatus: state.status.status
+});
+
 const mapDispatchToProps = dispatch => ({
-  createArticle: (article, token) => dispatch(createArticle(article, token))
+  createArticle: (article, token) => dispatch(createArticle(article, token)),
+  setActionInProgress: () => dispatch(setActionStatus("IN_PROGRESS"))
 });
 
 export default connect(
-  undefined,
+  mapStateToProps,
   mapDispatchToProps
 )(CreateBlogPostPage);
