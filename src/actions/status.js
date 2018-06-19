0d05c478ad3516bdd1f6c208115c70615f1fbe9f
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-19 14:40:11 +0200

    Create `status` reducer and actions

---
+++ src/actions/status.js
@@ -0,0 +1,11 @@
+export const actionInProgress = () => {
+  type: "IN_PROGRESS";
+};
+
+export const actionSuccessful = () => {
+  type: "SUCCESSFUL";
+};
+
+export const actionFailed = () => {
+  type: "FAILED";
+};
---
+++ src/reducers/status.js
@@ -0,0 +1,12 @@
+export default (state = {}, action) => {
+  switch (action.type) {
+    case "IN_PROGRESS":
+      return { status: "Action progress" };
+    case "SUCCESSFUL":
+      return { status: "Action successful" };
+    case "FAILED":
+      return { status: "Action failed" };
+    default:
+      return state;
+  }
+};
