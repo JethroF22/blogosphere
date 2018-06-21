130e00735dbd0558874f0bba27a33313d0d9dd09
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-21 19:44:54 +0200

    Fetch posts when component mounts

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { getPosts } from "../actions/blog";
 
-const HomePage = () => (
-  <div>
-    <h1>Welcome to Blogosphere!</h1>
-    <Link to="/register">Create Account</Link>
-    <Link to="/login">Login</Link>
-  </div>
-);
+const token = localStorage.getItem("token");
+
+class HomePage extends Component {
+  componentDidMount() {
+    if (!this.props.posts) {
+      this.props.getPosts();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Blogosphere</h1>
+        {!token ? (
+          <div>
+            <Link to="/register">Create Account</Link>
+            <br />
+            <Link to="/login">Login</Link>
+            <br />
+          </div>
+        ) : (
+          <div>
+            <Link to="/blog/create">Create Post</Link>
+            <br />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  posts: state.blog.posts
+});
+
+const mapStateToProps = dispatch => ({
+  getPosts: () => dispatch(getPosts())
+});
 
 export default HomePage;
