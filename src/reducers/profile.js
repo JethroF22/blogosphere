13a5a06b12e327c06af203ae249f3a0f32b8cd5e
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-07-12 01:03:39 +0200

    Change names for action types

--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -1,12 +1,12 @@
 export default (state = {}, action) => {
   switch (action.type) {
-    case "SET_DETAILS":
+    case "SET_PROFILE_DETAILS":
       return {
         ...state,
         photo: action.photo,
         bio: action.bio
       };
-    case "CLEAR_DETAILS":
+    case "CLEAR_PROFILE_DETAILS":
       return {};
     default:
       return state;
