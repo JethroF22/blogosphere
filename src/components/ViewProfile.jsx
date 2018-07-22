da5791a395508a97b55e4289d5cf1fc75f021f28
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-07-22 20:36:29 +0200

    Clear profile when `ViewProfile` component unmounts

--- src/components/ViewProfile.jsx
+++ src/components/ViewProfile.jsx
@@ -1,27 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-import { getProfile } from "../actions/profile";
+import { getProfile, clearProfileDetails } from "../actions/profile";
 
 class ViewProfile extends Component {
   componentDidMount() {
-    if (!this.props.profile.photo && !this.props.profile.bio) {
-      const username = this.props.match.params.username;
-      console.log(username);
-      this.props.getProfile(username).then(() => {
-        if (this.props.actionStatus === "Action successful") {
-          console.log("Success");
-        } else {
-          console.log("Error");
-        }
-      });
-    }
+    const username = this.props.match.params.username;
+    console.log(username);
+    this.props.getProfile(username).then(() => {
+      if (this.props.actionStatus === "Action successful") {
+        console.log("Success");
+      } else {
+        console.log("Error");
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.clearProfile();
   }
 
   render() {
     return (
       <div>
         <h1>{this.props.match.params.username}</h1>
+        <Link to="/">Home</Link>
         <img src={this.props.profile.photo} height="400" width="400" />
         <p>{this.props.profile.bio}</p>
       </div>
@@ -35,7 +39,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  getProfile: username => dispatch(getProfile(username))
+  getProfile: username => dispatch(getProfile(username)),
+  clearProfile: () => dispatch(clearProfileDetails())
 });
 
 export default connect(
