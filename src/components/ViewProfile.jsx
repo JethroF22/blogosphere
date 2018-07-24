c435b7daed66adbf5c74e8eb90329ce0eb26954a
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-07-24 02:30:35 +0200

    Display author's published posts on profile page

--- src/components/ViewProfile.jsx
+++ src/components/ViewProfile.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 
-import { getProfile, clearProfileDetails } from "../actions/profile";
+import ArticleList from "./ArticleList";
+import {
+  getProfile,
+  clearProfileDetails,
+  getPostsByAuthor
+} from "../actions/profile";
 
 class ViewProfile extends Component {
   componentDidMount() {
     const username = this.props.match.params.username;
-    console.log(username);
     this.props.getProfile(username).then(() => {
       if (this.props.actionStatus === "Action successful") {
-        console.log("Success");
+        this.props.getPosts(username);
       } else {
         console.log("Error");
       }
@@ -25,9 +28,20 @@ class ViewProfile extends Component {
     return (
       <div>
         <h1>{this.props.match.params.username}</h1>
-        <Link to="/">Home</Link>
         <img src={this.props.profile.photo} height="400" width="400" />
-        <p>{this.props.profile.bio}</p>
+        {this.props.profile.bio && <p>{this.props.profile.bio}</p>}
+        {this.props.profile.followers && (
+          <p>
+            {this.props.followersCount} follower{this.props.followersCount > 0
+              ? "s"
+              : ""}
+          </p>
+        )}
+        {this.props.publishedPosts ? (
+          <ArticleList posts={this.props.publishedPosts} />
+        ) : (
+          "No posts to display"
+        )}
       </div>
     );
   }
@@ -35,12 +49,15 @@ class ViewProfile extends Component {
 
 const mapStateToProps = state => ({
   profile: state.profile,
-  actionStatus: state.status.status
+  actionStatus: state.status.status,
+  followersCount: state.profile.followers ? state.profile.followers.length : 0,
+  publishedPosts: state.profile.publishedPosts
 });
 
 const mapDispatchToProps = dispatch => ({
   getProfile: username => dispatch(getProfile(username)),
-  clearProfile: () => dispatch(clearProfileDetails())
+  clearProfile: () => dispatch(clearProfileDetails()),
+  getPosts: username => dispatch(getPostsByAuthor(username))
 });
 
 export default connect(
