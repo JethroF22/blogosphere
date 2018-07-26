252f701f28fd0ce38232b359e7bece0354f80741
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-07-26 16:25:06 +0200

    Configure and apply base styles

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,7 @@ import configureStore from "./store/configureStore";
 import AppRouter from "./routes/AppRouter";
 import { tokenAuthentication } from "./actions/auth";
 import LoadingPage from "./components/LoadingPage";
+import "./styles/styles.scss";
 
 const store = configureStore();
 const token = localStorage.getItem("token");
