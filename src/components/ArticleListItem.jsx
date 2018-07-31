3c4b2d9808c5c6433a2a589d301c0b9edf280a9c
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-07-31 02:34:41 +0200

    Style `HomePage`, `ArticleList` and `ArticleListItem`

--- src/components/ArticleListItem.jsx
+++ src/components/ArticleListItem.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import moment from "moment";
 
 const ArticleListItem = props => (
-  <div>
-    <h3>{props.post.title}</h3>
-    <img src={props.post.coverPhotoURL} height="400px" width="400px" />
+  <div className="uk-card uk-card-default uk-card-hover uk-margin-medium-bottom">
+    <div className="uk-card-header">
+      <h3 className="article-list__title">{props.post.title}</h3>
+      <p className="article-list__subtitle">
+        Created on {moment(props.post.createdAt).format("MMMM Do YYYY")}
+      </p>
+    </div>
+    <div className="uk-card-media-bottom">
+      <img
+        className="article-list__image"
+        uk-img={`dataSrc: ${props.post.coverPhotoURL}`}
+      />
+    </div>
   </div>
 );
 
