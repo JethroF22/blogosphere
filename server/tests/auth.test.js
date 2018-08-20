34b0c52c8f1e9b2ea7983a726798464c1560d7ed
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-08-20 20:19:31 +0200

    Remove `POST` describe block

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -12,48 +12,46 @@ describe("/auth", () => {
       User.remove({}).then(() => done());
     });
 
-    describe("POST", () => {
-      it("should create a new user", function(done) {
-        this.timeout(0);
+    it("should create a new user", function(done) {
+      this.timeout(0);
 
-        request(app)
-          .post("/auth/register")
-          .send(users[0])
-          .expect(200)
-          .expect(res => {
-            expect(res.body.user.token).to.be.a("string");
-            expect(res.body.user.username).to.equal(users[0].username);
-            expect(res.body.user.email).to.equal(users[0].email);
-          })
-          .end(err => {
-            if (err) {
-              return done(err);
-            }
+      request(app)
+        .post("/auth/register")
+        .send(users[0])
+        .expect(200)
+        .expect(res => {
+          expect(res.body.user.token).to.be.a("string");
+          expect(res.body.user.username).to.equal(users[0].username);
+          expect(res.body.user.email).to.equal(users[0].email);
+        })
+        .end(err => {
+          if (err) {
+            return done(err);
+          }
 
-            User.findOne({ email: users[0].email })
-              .then(user => {
-                expect(user.password).to.not.equal(users[0].password);
-                done();
-              })
-              .catch(err => done(err));
-          });
-      });
+          User.findOne({ email: users[0].email })
+            .then(user => {
+              expect(user.password).to.not.equal(users[0].password);
+              done();
+            })
+            .catch(err => done(err));
+        });
+    });
 
-      it("should return vaildation errors for invalid requests", done => {
-        const email = "invalidemail";
-        const password = "short";
+    it("should return vaildation errors for invalid requests", done => {
+      const email = "invalidemail";
+      const password = "short";
 
-        request(app)
-          .post("/auth/register")
-          .send({ email })
-          .expect(400)
-          .expect(res => {
-            expect(res.error.errors).to.have.property("username");
-            expect(res.error.errors).to.have.property("password");
-            expect(res.error.errors).to.have.property("email");
-          })
-          .end(() => done());
-      });
+      request(app)
+        .post("/auth/register")
+        .send({ email })
+        .expect(400)
+        .expect(res => {
+          expect(res.error.errors).to.have.property("username");
+          expect(res.error.errors).to.have.property("password");
+          expect(res.error.errors).to.have.property("email");
+        })
+        .end(() => done());
     });
   });
 });
